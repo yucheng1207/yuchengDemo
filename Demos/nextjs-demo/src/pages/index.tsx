d2e60faf1bb84c60ae1bdcb25f55babbc28baf64
Fred <zhangyucheng1207@gmail.com>
2021-06-17 15:04:55 +0800

    添加sass

--- Demos/nextjs-demo/src/pages/index.tsx
+++ Demos/nextjs-demo/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import styles from '../styles/Home.module.css'
+import styles from './index.module.scss'
 
 export default function Home() {
 	const router = useRouter()
