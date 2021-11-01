0bf0117512cf8fe155f49f14c73a3df2159f7773
Fred <zhangyucheng1207@gmail.com>
2021-11-01 17:43:59 +0800

    three-js-demo: 绘制正方体

--- Demos/three-js-demo/README.md
+++ Demos/three-js-demo/README.md
@@ -4,7 +4,7 @@ three.js 学习 demo，使用 create-react-app 脚手架搭建
 
 # 参考文档：
 
--   [官网](https://threejs.org/docs/index.html#manual/en/introduction/Drawing-lines)
+-   [官网](https://threejs.org/docs/index.html#manual/zh/introduction/Creating-a-scene)
 
 # 调试
 
--- Demos/three-js-demo/src/components/ThreeBuilding/index.tsx
+++ Demos/three-js-demo/src/components/ThreeBuilding/index.tsx
@@ -15,15 +15,45 @@ const ThreeBuilding: React.FunctionComponent<Props> = (props) => {
 	const [scene, setScene] = useState<THREE.Scene>()
 	const [camera, setCamera] = useState<THREE.PerspectiveCamera>()
 	const [renderer, setRenderer] = useState<THREE.WebGLRenderer>()
-	const renderThree = useCallback(() => {
-		renderer && scene && camera && renderer.render(scene, camera);
-	}, [renderer, scene, camera])
+	const [cube, setCube] = useState<THREE.Mesh<THREE.BoxGeometry, THREE.MeshLambertMaterial>>()
+
+
+	/**
+	 * 初始化渲染器
+	 */
+	const initRender = (containerDiv: HTMLDivElement, width: number, height: number) => {
+		// 创建渲染器
+		const renderer = new THREE.WebGLRenderer({
+			antialias: true // 抗锯齿开启
+		}); // 创建渲染器
+		renderer.setSize(width, height); // 设置渲染器宽度和高度
+		renderer.setClearColor('#000000', 1.0); // 设置背景颜色
+		renderer.setPixelRatio(window.devicePixelRatio); // 设置设备像素比
+		containerDiv.appendChild(renderer.domElement); // 将整个场景推入我们要显示的元素中
+		return renderer
+	}
+
+	/**
+	 * 创建相机：相机的作用就类似于人眼，决定了观察的视角和位置；
+	 * ThreeJS 框架同样提供了多种相机，比较常用有两种，分别为PerspectiveCamera透视投影相机和OrthographicCamera正交投影相机；
+	 * 其中和人眼观察效果一致的是透视投影相机，用其观察物体时能获得近大远小的效果
+	 */
+	const initCamera = (width: number, height: number) => {
+		const origPoint = new THREE.Vector3(0, 0, 0); // 原点
+		const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000); // 第一个参数 -> 视野角度（单位：度）  第二个参数是长宽比 第三个是近截面 第四个是远截面
+		camera.position.set(200, 400, 600); // 设置相机位置
+		camera.up.set(0, 1, 0); // 设置相机正方向
+		camera.lookAt(origPoint); // 设置相机视点
+		return camera
+	}
 
 	const createCube = useCallback((scene) => {
-		const geometry = new THREE.BoxGeometry(1, 2, 1, 4); // 绘制一个立方体，擦书相当于定点位置 （three自带的对象）
-		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // 定义材质 我们这里用简单的颜色，其他的属性可以写入对象，就可以更改材质
-		const cube = new THREE.Mesh(geometry, material); // 我们用到网格将 定义的材质用到定义的立方题上生成cube
-		scene && scene.add(cube); // 将我们生成的cube放到场景中 
+		const geometry = new THREE.BoxGeometry(100, 100, 100); // 正方体轮廓
+		const material = new THREE.MeshLambertMaterial({ color: 0xff0000 }); // 正方体材质
+		const cube = new THREE.Mesh(geometry, material);
+		cube.position.set(0, 0, 0);
+		scene && scene.add(cube); // 将生成的cube放到场景中
+		return cube
 	}, [])
 
 	const createLine = useCallback((scene) => {
@@ -35,30 +65,62 @@ const ThreeBuilding: React.FunctionComponent<Props> = (props) => {
 		const geometry = new THREE.BufferGeometry().setFromPoints(points);
 		const line = new THREE.Line(geometry, material)
 		scene && scene.add(line)
+		return line
 	}, [])
+
+	/**
+	 * 初始化光源
+	 */
+	const initLight = useCallback((scene) => {
+		//点光源
+		const pointLight = new THREE.PointLight(0xffffff, 1, 2000);
+		pointLight.position.set(70, 112, 98);
+		//环境光
+		const ambientLight = new THREE.AmbientLight(0x333333);
+		scene.add(pointLight)
+		scene.add(ambientLight)
+	}, [])
+
+	const render = useCallback(() => {
+		if (renderer && scene && camera && cube) {
+			renderer.clear();
+			renderer.render(scene, camera);
+			cube.rotation.x += 0.005;
+			cube.rotation.y += 0.005;
+			requestAnimationFrame(render);
+		} else if (renderer && scene && camera) {
+			renderer.render(scene, camera);
+		}
+	}, [renderer, camera, scene, cube])
 
 	const init = useCallback(() => {
 		const containerDiv = containerRef.current
 		if (containerDiv) {
 			const width = containerDiv.clientWidth
 			const height = containerDiv.clientHeight
-			const renderer = new THREE.WebGLRenderer({ antialias: true }); // 创建渲染器。讲道理我还没有看这个参数是什么意思。 但是官网中有一个测试浏览器是否可以使用WebGL的方法，需要用到的可看一下
-			renderer.setSize(width, height); // 将渲染器的长宽 设置为我们要显示的容器长宽
-			containerDiv.appendChild(renderer.domElement); // 将整个场景推入我们要显示的元素中
-			const scene = new THREE.Scene() //创建场景
-			const camera = new THREE.PerspectiveCamera(75, width / height, 1, 500); // 创建相机  这些参数在官网中都有指出  第一个参数 75 -> 视野角度（单位：度）  第二个参数是长宽比 第三个是近截面 第四个是远截面
-			camera.position.set(0, 0, 100);
-			camera.lookAt(0, 0, 0);
 
+			// 创建渲染器
+			const renderer = initRender(containerDiv, width, height)
+
+			// 创建相机
+			const camera = initCamera(width, height)
+
+			// 创建场景
+			const scene = new THREE.Scene();
+			// 创建一个正方体
+			const cube = createCube(scene)
+			// 初始化光源
+			initLight(scene)
+
+			// 同步到state
 			setScene(scene)
 			setCamera(camera)
 			setRenderer(renderer)
+			setCube(cube)
 
-			createLine(scene)
-			createCube(scene)
-			renderer.render(scene, camera);
+			render()
 		}
-	}, [createLine, createCube])
+	}, [initLight, createCube, render])
 
 	return <div className={styles.threeBuilding} ref={containerRef}>
 	</div>;
