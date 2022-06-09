55ebda3205604d4831b15016ce5e0348067b2348
yucheng1207 <839884927@qq.com>
2022-06-09 17:02:21 +0800

    react-hook-demo: antd按需加载

--- Demos/react-hook-demo/config/webpack.config.js
+++ Demos/react-hook-demo/config/webpack.config.js
@@ -421,6 +421,11 @@ module.exports = function (webpackEnv) {
                       },
                     },
                   ],
+                  ['import',{  // 导入一个插件
+                    libraryName: 'antd',   // 暴露的库名
+                    libraryDirectory: 'lib',
+                    style: 'css' // 直接将ants样式文件动态编译成行内样式插入，就不需要每次都导入
+                  }],
                   isEnvDevelopment &&
                     shouldUseReactRefresh &&
                     require.resolve('react-refresh/babel'),
