3a56b5da19463b368725542b273ac69541e065a8
zhangyucheng-ones <zhangyucheng@ones.ai>
2022-07-13 11:24:05 +0800

    ones-demo: add @ones-design/icons

--- Demos/ones-demo/src/components/OnesDesignTest/index.tsx
+++ Demos/ones-demo/src/components/OnesDesignTest/index.tsx
@@ -1,10 +1,19 @@
 
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Button } from '@ones-design/core';
 
 const OnesDesignTest: React.FunctionComponent<{}> = function (props) {
+	const [loading, setLoading] = useState(false)
+	const handleClick = useCallback(() => {
+		setLoading(true)
+		setTimeout(() => {
+			setLoading(false)
+		}, 5000)
+	}, [])
 	return (
-		<div>
-			hello
+		<div style={{fontFamily: 'PingFang SC,"微软雅黑",Arial'}}>
+			<Button loading={loading} onClick={handleClick}>123</Button>
+			<Button type="primary" disabled={false} loading={loading} onClick={handleClick}>123</Button>
 		</div>
 	)
 }
